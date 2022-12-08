40883a66d8f267c50296854ac895d0a5e0fe140f
Wouter Vernaillen <wouter@vernaillen.com>
2022-12-08 13:52:06 +0100

    removed files that were moved to av-theme + upgraded av-theme

--- cypress.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'cypress'
-
-export default defineConfig({
-  video: false,
-  e2e: {
-    baseUrl: 'https://anneleen.netlify.com/',
-  },
-})
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,14 @@
-import { resolve } from 'path'
-
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxt/content',
-    '@nuxt/image-edge',
-    '@nuxtjs/google-fonts',
-    '@nuxtjs/i18n',
-    '@nuxtjs/tailwindcss',
-    'nuxt-icon',
-    '@nuxthq/studio',
+  extends: [
+    'av-theme',
   ],
-  css: [
-    '@/assets/css/main.css',
-    '@/assets/css/markdown.css',
-    '@/assets/css/prose.css',
-    'animate.css/animate.min.css',
-  ],
+  image: {
+    provider: 'twicpics',
+    twicpics: {
+      baseURL: 'https://vernaillen.twic.pics/anneleen',
+    },
+  },
   head: {
     htmlAttrs: {
       lang: 'en',
@@ -26,32 +18,7 @@ export default defineNuxtConfig({
       { name: 'description', content: 'Anneleen Vernaillen. Visual Artist.' },
     ],
   },
-  image: {
-    provider: 'twicpics',
-    twicpics: {
-      baseURL: 'https://vernaillen.twic.pics/anneleen',
-    },
-  },
-  content: {
-    documentDriven: true,
-    markdown: {
-      remarkPlugins: ['remark-directive-rehype'],
-    },
-  },
   generate: {
     routes: ['/', '/about', '/art-initiation', '/ceramics', '/contact', '/graphic-design', '/news-archive', '/wall-art'],
-  },
-  i18n: {
-    strategy: 'prefix_and_default',
-    defaultLocale: 'en',
-    langDir: './locales/',
-    locales: [
-      { code: 'en', iso: 'en-US', file: 'en-US.json' },
-      { code: 'nl', iso: 'en-BE', file: 'nl-BE.json' },
-    ],
-    detectBrowserLanguage: {
-      useCookie: true,
-      redirectOn: 'root', // recommended
-    },
   },
 })
