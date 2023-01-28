daa58d457a2174a629ceb66c7ef93a4724ebacf7
Wouter Vernaillen <wouter@vernaillen.com>
2023-01-28 15:20:05 +0100

    page transition update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,8 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   extends: [
-    'av-theme',
+    '../av-theme',
   ],
-  app: {
-    layoutTransition: { name: 'layout', mode: 'out-in' }
-  },
   image: {
     provider: 'twicpics',
     twicpics: {
