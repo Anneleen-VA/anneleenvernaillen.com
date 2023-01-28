831371549b9b65762a91f5f9825f54fabb1d40b2
Wouter Vernaillen <wouter@vernaillen.com>
2023-01-28 15:30:49 +0100

    fix extend from av-theme

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   extends: [
-    '../av-theme',
+    'av-theme',
   ],
   image: {
     provider: 'twicpics',
