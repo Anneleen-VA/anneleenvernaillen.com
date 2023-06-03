e129746bacbaf0fc8714ce77bc6c2409cfe14cfc
Wouter Vernaillen <wouter@vernaillen.com>
2023-06-03 09:42:17 +0200

    test node 16 for compatibility with StackBlitz

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,3 @@
-import { logger } from '@nuxt/kit'
-import { version } from './package.json'
-
-logger.success(`nuxt app v${version} for anneleenvernaillen.com created by Wouter Vernaillen`)
-
 export default defineNuxtConfig({
   extends: [
     './app',
