986ed9f597575879ba105c570d9961fcae649ba2
Wouter Vernaillen <wouter@vernaillen.com>
2023-09-10 18:45:40 +0200

    magazine

---
+++ app/pages/magazine.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <NuxtLayout>
+      <div class="bg-primary bg-opacity-[35%] w-full">
+        <div class="container max-w-screen-xl my-12 sm:my-16 xl:my-20 py-10">
+          <PdfViewer />
+        </div>
+      </div>
+    </NuxtLayout>
+  </div>
+</template>
